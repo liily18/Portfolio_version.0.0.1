a3a5572f998cc93bd9b07ce196a432d11f787433
liily <lilyanasa.or@gmailcom>
2024-09-19 22:20:12 -0300

    portafolio

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+    document.getElementById("enviarmsj").addEventListener("click", function(){
+        alert("Gracias por Contactarnos, Pronto nos pondremos en contacto contigo.")
+    }
+)
+
+}
+)
